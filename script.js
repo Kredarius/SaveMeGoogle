4f1f98176958be809372a0223778b63f8e1ab5a7
Radek Stellner <95450518+Kredarius@users.noreply.github.com>
2023-02-09 09:09:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 btn.addEventListener("click", run);
 
+const searchText = input.value;
+
 async function run() {
+    
     loadingIndicatorStart();
-    let response = getResponse();
+    
+    let response = await getResponse;
+    let resultDiv = await makeResult(response);
+    document.append(resultDiv);
+    
+    loadingIndicatorEnd();
 }
 
 async function getResponse() {
     const KEY = "AIzaSyDyl1MZJ_Pgw3KYNeoHipZK1rFa8wZP_Wg";
     const CX = "715a88c4d97dc4f10";
-    const searchText = input.value;
     let url = new URL('https://www.googleapis.com/customsearch/v1?');
 
     url.searchParams.set('q', searchText);
@@ -18,41 +25,50 @@ async function getResponse() {
     const response = await fetch(url, {
         method: "GET",
     });
+    
     return response;
 }
 
-async function getData(response) {
-    
+async function makeResult(response) {
+    let resultDiv = makeResultDiv();
+    let resultContent;
+    if (result.ok) {
+        let data = await getData();
+        resultContent = makeListWithDownload(data);
+    } else {
+        resultContent = makeFailureMessage(response.status);
+    }
+    resultDiv.append(resultContent);
+    return resultDiv;
 }
 
-    result = makeResultDiv();
+async function getData(response) {
+    const json = await response.json();
+    return json;
+}
+function makeListWithDownload(data) {
+    let listNode = makeList(data);
+    listNode.after(makeJSONDownload(data));
+    return listNode;
+} 
+    
+function makeList(data) {
+    let ul = document.createElement("ul");
+    
+    for (item of json.items) {
+        let li = document.createElement("li");
+        li.innerText = item.title + " ";
 
-    if (+response.status === 200) {   
-        const json = await response.json();
-
-        let ul = document.createElement("ul");
-        result.append(ul);
-        for (item of json.items) {
-            let li = document.createElement("li");
-            li.innerText = item.title + " ";
-
-            let link = document.createElement("a");
-            link.innerHTML = "Link";
-            link.href = item.link;
-            li.append(link);
-            ul.append(li);
-        }
-        
-        result.append(makeJSONDownload(json.items));
-
-    } else {
-        result.append(makeFailureMessage(searchText, response.status))
+        let link = document.createElement("a");
+        link.innerHTML = "Link";
+        link.href = item.link;
+        li.append(link);
+        ul.append(li);
     }
+    
+    return ul;
+}
 
-    document.body.append(result);
-    loadingIndicatorEnd();
-    }
-function 
 function makeJSONDownload(data) {
     let downloadLinkAnchor = document.createElement("a");
     let blob = new Blob([JSON.stringify(data)], {type: 'application/json'});
@@ -70,7 +86,7 @@ function makeResultDiv() {
     return resultDiv;
 }
 
-function makeFailureMessage(searchText, status) {
+function makeFailureMessage(status) {
     let paragraph = document.createElement("p");
     paragraph.innerText = `Search "${searchText}" failed with status ${status}`;
     return paragraph;
